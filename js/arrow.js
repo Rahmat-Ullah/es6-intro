5c6b6045684776a64e7d631002fdb96cd71ad6a0
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-10-21 06:35:37 +0600

    arrow function in es6 practiced

---
+++ js/arrow.js
@@ -0,0 +1,33 @@
+function add(first, second){
+    const total = first + second;
+    return total;
+}
+
+const number = 34;
+
+const add1 = function add1(first, second){
+    const total = first + second;
+    return total;
+}
+const add2 = function (first, second){
+    const total = first + second;
+    return total;
+}
+
+function add(first, second){
+    const total = first + second;
+    return total;
+}
+
+function add4(first, second){
+    return first + second;
+}
+
+const add5 = function (first, second){
+    return first + second;
+}
+
+const add6 = (first, second) => first + second;
+
+const result = add(12, 34);
+console.log(result);
